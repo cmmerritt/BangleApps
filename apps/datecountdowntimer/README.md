130e96f9785536f575749e202fe0c2573e8f751a
cmmerritt <christiane.merritt@gmail.com>
2022-05-30 23:47:24 -0700

    add app, metadata and readme

---
+++ apps/datecountdowntimer/README.md
@@ -0,0 +1,6 @@
+A countdown timer to keep track of days, hours, minutes, and seconds until a given date. 
+
+## Future development
+* User enters date using watch interface.
+* Customizable alert when timer ends.
+* Display changes depending on how much time remains.
---
+++ apps/datecountdowntimer/app.js
@@ -0,0 +1,49 @@
+// code can be tested by using the Bangle.js 2 watch emulator with the Espruino IDE: https://www.espruino.com/ide/ 
+
+// create proof of concept with static date
+// then take user input for targetDate
+
+const Layout = require("Layout");
+
+const targetDate = "Oct 31, 2022";
+
+const goalTime = new Date(targetDate).getTime();
+
+const second = 1000;
+const minute = second * 60;
+const hour = minute * 60;
+const day = hour * 24;
+
+function displayCountDown() {
+  const currentTime = new Date();
+  
+  let timeLeft = goalTime - currentTime;
+  
+  if(timeLeft < 0) {
+    E.showMessage("It's Your Day!", "Countdown Timer");
+    Bangle.buzz();
+  }
+  
+  const days = Math.floor(timeLeft / day);
+  const hours = Math.floor((timeLeft % day) / hour);
+  const minutes = Math.floor((timeLeft % hour) / minute);
+  const seconds = Math.floor((timeLeft % minute) / second);
+  
+  const timeStringDaysHours = days + " days " + hours + " hours";
+  const timeStringMinutesSeconds = minutes + " minutes " + seconds + " seconds";
+  
+  const untilString = "until " + targetDate;
+  
+  var layout = new Layout( {
+  type:"v", c: [
+    {type:"txt", font:"6x8", label:timeStringDaysHours, id:"dayshours" },
+    {type:"txt", font:"6x8", label:timeStringMinutesSeconds, id:"minutesseconds" },
+    {type:"txt", font:"6x8", label:untilString, id:"until" }
+  ]
+  });
+  g.clear();
+  layout.render();
+  Bangle.setLCDPower(1);
+}
+
+displayCountDown();
